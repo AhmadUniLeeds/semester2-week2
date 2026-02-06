684e070d2d90f7473e153ddf5d269984dfc18582
AhmadUniLeeds <ctvk0892@leeds.ac.uk>
2026-02-06 09:17:35 +0000

    Completed tasks for 2.2.1

--- session_1/2_practice/code.sql
+++ session_1/2_practice/code.sql
@@ -9,3 +9,8 @@
 
 
 -- write your sql code here
+
+-- SELECT title, name, loan_date FROM Books LEFT JOIN Loans ON Books.id = loans.id LEFT JOIN Members ON Loans.member_id = Members.id;
+-- SELECT title, name FROM LibraryBranch LEFT JOIN Books ON LibraryBranch.id = Books.branch_id;
+-- SELECT name, COUNT(*) FROM LibraryBranch LEFT JOIN Books ON LibraryBranch.id = Books.branch_id GROUP BY branch_id;
+-- SELECT name, COUNT(*) FROM LibraryBranch LEFT JOIN Books ON LibraryBranch.id = Books.branch_id GROUP BY branch_id HAVING COUNT(*) > 7;
